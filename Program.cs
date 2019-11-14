c873fcdf5f803222ac9364b46b70d7c67d6fd4b0
roshangurung <r.gurung1992@gmail.com>
2019-11-14 12:13:00 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Text;
+
+
+namespace String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("UNITED STATES");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            builder.Replace('-', '+');
+            builder.Remove(0, 10);
+            builder.Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+          
+        }
+    }
+}
+///////Output: ---------UNITED STATES++++++++++/////
